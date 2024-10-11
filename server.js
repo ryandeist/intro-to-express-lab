d328c4512bc7c669f040f4386f9739fc5b198e91
ryandeist <ryandeist@gmail.com>
2024-10-11 16:35:05 -0400

    Ex. 3 Complete

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
-// import express
+// Import express
 const express = require('express');
 
 const app = express();
 
-//middleware functions
+//Data
+const collectibles = [
+    { name: 'shiny ball', price: 5.95 },
+    { name: 'autographed picture of a dog', price: 10 },
+    { name: 'vintage 1970s yogurt SOLD AS-IS', price: 0.99 }
+  ];
+
+//Middleware functions
 
 //Routes
+//Exercise 1
 app.get("/greetings/:username", (req, res) => {
-    const username = req.params.username
+    const username = req.params.username;
 
     res.send(`<h1>Greetings ${username}</h1>`);
 });
 
+// Exercise 2
 app.get("/roll/:diceRoll", (req, res) => {
     const diceRoll = Number(req.params.diceRoll);
 
@@ -20,6 +29,16 @@ app.get("/roll/:diceRoll", (req, res) => {
         res.send(`<h1>You rolled a ${randomNum}</h1>`);
     } else {
         res.send(`<h1>You must specify a number.</h1>`);
+    };
+});
+
+// Exercise 3
+app.get("/collectibles/:collectiblesIndex", (req, res) => {
+    const collectiblesIndex = Number(req.params.collectiblesIndex);
+    if (collectiblesIndex < collectibles.length) {
+        res.send(`<h1>The ${collectibles[collectiblesIndex].name} is available for $${collectibles[collectiblesIndex].price}.</h1>`);
+    } else {
+        res.send(`<h1>This item is not in stock. Check back soon.</h1>`);
     };
 });
 
